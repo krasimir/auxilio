73c4da83f9f964c433e048d0de9ad0cba367e311
Krasimir Tsonev <krasimir@outset.ws>
2013-03-31 00:10:27 +0200

    trying to duplicate the native device metrics feature

--- src/js/background.js
+++ src/js/background.js
@@ -19,6 +19,9 @@ chrome.extension.onConnect.addListener(function (port) {
        	switch(port.name) {
 			case "ResponsiveResolution":
 				responsiveResolutionPort = port;
+				responsiveResolutionPort.onDisconnect.addListener(function() {
+					responsiveResolutionPort = null;
+				});
 				switch(message.type) {
 					case "update-window-size":
 						if(currentWindow) {
@@ -26,6 +29,7 @@ chrome.extension.onConnect.addListener(function (port) {
 						        width: message.data.w,
 						        height: message.data.h
 						    });
+						    // changeDeviceMetrics(message.data.w, message.data.h);
 						}
 					break;
 					case "give-me-current-window-size":
@@ -56,3 +60,33 @@ chrome.extension.onConnect.addListener(function (port) {
 chrome.windows.getCurrent({}, function(w) {
     currentWindow = w;
 });
+
+// --------------------------------------------------------------------------------- Responsive / Device metrics
+// http://stackoverflow.com/questions/15618923/in-google-chrome-what-is-the-extension-api-for-changing-the-useragent-and-devic
+// var changeDeviceMetrics = function(w, h) {
+
+// 	var protocolVersion = '1.0';
+
+// 	var onAttach = function(tabId) {
+// 		chrome.debugger.sendCommand({
+// 		    tabId: tabId
+// 		}, "Page.setDeviceMetricsOverride", {
+// 		    width: w,
+// 		    height: h,
+// 		    fontScaleFactor: 1,
+// 		    fitWindow: false
+// 		}, function(response) {
+// 		    console.log("on setDeviceMetricsOverride", response);
+// 		});
+// 	}
+
+// 	chrome.windows.getCurrent(function(win) {
+// 		chrome.tabs.getSelected(win.id, function(tab) {
+// 			chrome.debugger.detach({tabId:tab.id}, function() {
+// 				console.log("on detach");
+// 				chrome.debugger.attach({tabId:tab.id}, protocolVersion, onAttach.bind(null, tab.id));
+// 			});
+// 			// onAttach(tab.id)
+// 		});
+// 	});
+// }
