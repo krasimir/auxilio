27010e1a88e75b2222cd7bed1864bc41eda192a3
Krasimir Tsonev <krasimir@outset.ws>
2013-04-02 01:15:48 +0300

    experimenting with device metrics

---
+++ src/browseraction-devicemetrics/popup.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+	document.getElementById("click-me").onclick = function() {
+		changeDeviceMetrics(400, 600);
+	}
+}
+
+// resizing viewport
+// --------------------------------------------------------------------------------- Responsive / Device metrics
+// http://stackoverflow.com/questions/15618923/in-google-chrome-what-is-the-extension-api-for-changing-the-useragent-and-devic
+var changeDeviceMetrics = function(w, h) {
+	console.log("changeDeviceMetrics", w, h);
+
+	var protocolVersion = '1.0';
+
+	var onAttach = function(tabId) {
+		console.log("onAttach");
+		chrome.debugger.sendCommand({
+		    tabId: tabId
+		}, "Page.setDeviceMetricsOverride", {
+		    width: w,
+		    height: h,
+		    fontScaleFactor: 1,
+		    fitWindow: false
+		}, function(response) {
+		    console.log("on setDeviceMetricsOverride", response);
+		});
+	}
+
+	chrome.windows.getCurrent(function(win) {
+		chrome.tabs.getSelected(win.id, function(tab) {
+			chrome.debugger.attach({tabId:tab.id}, protocolVersion, onAttach.bind(null, tab.id));			
+			// onAttach(tab.id)
+		});
+	});
+}
