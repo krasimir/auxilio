cb0093700370af33791c86224e7ec382310c710d
Krasimir Tsonev <krasimir@outset.ws>
2013-06-04 17:15:45 +0300

    Finishing autocompletion

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,72 @@
 // Available commands
 var Commands = {
+	l: function() {
+		App.clear('', true);
+	},
+	page: function() {
 
+	}
 }
 var CommandsDictionary = ["lose-yourself", "ls", "log", "love-chrome"];
+
 
 // The main application logic
 var App = {
 	init: function() {
 		this.suggest = $("#js-suggest");
 		this.command = $("#js-command");
+		this.output = $("#js-console-output-content");
 		this.matches = [];
 		this.defineKeyboardEvents();
+		this.prepareDictionary();
+	},
+	prepareDictionary: function() {
+		// adding commands from Commands
+		for(var i in Commands) {
+			var added = false;
+			for(var j=0; j<CommandsDictionary.length; j++) {
+				if(CommandsDictionary[j] == i) {
+					added = true;
+					j = CommandsDictionary.length;
+				}
+			}
+			if(!added) CommandsDictionary.push(i);
+		}
+		// sort the array by command length
+		for(var i=0; i<CommandsDictionary.length; i++) {
+			for(var j=0; j<CommandsDictionary.length; j++) {
+				if(CommandsDictionary[i].length < CommandsDictionary[j].length) {
+					var temp = CommandsDictionary[i];
+					CommandsDictionary[i] = CommandsDictionary[j];
+					CommandsDictionary[j] = temp;
+				}
+			}
+		}
 	},
 	defineKeyboardEvents:function() {
 		var self = this;
 		this.command.on("focus", function() {
 			self.command.on("keyup", function(e) {
 				var code = (e.keyCode ? e.keyCode : e.which);
-				if(self["key" + code]) {
-					self["key" + code]();
-				}
+				if(self["key" + code]) self["key" + code](e);
 				self.autocomplete();
 			});
+			self.command.on("keydown", function(e) {
+				var code = (e.keyCode ? e.keyCode : e.which);
+				if(self["key" + code]) self["key" + code](e);
+			});
 		});
-		this.command.on("blur", function() {
+		this.command.on("blur", function(e) {
+			self.command.off("keyup");
 			self.command.off("keydown");
 		});
 	},
 	// keyboard handlers
 	autocomplete: function() {
 		this.suggest.val("");
+		this.matches = [];
 		var commandStr = this.command.val();
 		if(commandStr == "") return;
-		this.matches = [];
 		for(var i=0; i<CommandsDictionary.length; i++) {
 			var suggestion = CommandsDictionary[i];
 			var re = new RegExp("^" + commandStr.toLowerCase() + ".*?");
@@ -49,9 +83,24 @@ var App = {
 			this.suggest.val(suggestionStr);
 		}
 	},
-	key13: function() {
+	key13: function(e) { // enter
 		var commandStr = this.command.val();
 		this.command.val("");
+		if(Commands[commandStr]) {
+			Commands[commandStr]();
+		}
+	},
+	key9: function(e) { // tab
+		e.preventDefault();
+		if(this.matches.length > 0) {
+			this.command.val(this.matches[0]);
+			this.autocomplete();
+		}
+	},
+	// console output
+	clear: function(str, clearPreviousContent) {
+		var previousContent = this.output.html();
+		this.output.html(clearPreviousContent ? str : previousContent + str);
 	}
 }
 
