90726e729470ee0ee38ace35751ee248617eb810
Krasimir Tsonev <krasimir@outset.ws>
2013-06-16 14:23:52 +0300

    Improving aliases

--- src/js/commands/tabs/newtab.js
+++ src/js/commands/tabs/newtab.js
@@ -1,14 +1,15 @@
 Commands.register("newtab", {
 	requiredArguments: 0,
-	format: '<pre>newtab\nnewtab [url]</pre>',
+	format: '<pre>newtab\nnewtab [url] [active (true | false)]</pre>',
 	run: function(args, callback) {
 		if(chrome && chrome.runtime) {
 			if(args[0]) {
-				var url = args[0];
+				var url = args.shift();
+				var active = args.length > 0 ? args.shift() : false;
 				if(url.indexOf("http") == -1 && url.indexOf("data:image") == -1) {
 					url = "http://" + url;
 				}
-				chrome.runtime.sendMessage({type: "newtab", url: url}, function() {
+				chrome.runtime.sendMessage({type: "newtab", url: url, active: active}, function() {
 					callback();
 				});
 			}else {
