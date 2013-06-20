e31a5417c58124a20c367a5751709d164259e444
Krasimir Tsonev <krasimir@outset.ws>
2013-06-20 09:25:49 +0300

    Adding shell command

--- src/js/app-commands.js
+++ src/js/app-commands.js
@@ -458,6 +458,25 @@ var request = function(url, callback) {
 	}
 	xhr.send();
 }
+Commands.register("shell", {
+	requiredArguments: 1,
+	format: '<pre>shell [command]</pre>',
+	lookForQuotes: false,
+	concatArgs: true,
+	run: function(args, callback) {
+		var command = args.join(" ");
+		if(Shell.connected()) {
+			Shell.send(command, function(res) {
+				callback(res);
+			});
+		} else {
+			exec("error Sorry, the corresponding nodejs module is not running.");
+		}
+	},
+	man: function() {
+		return 'Executes shell command.';
+	}	
+})
 Commands.register("alias", {
 	requiredArguments: 0,
 	lookForQuotes: false,
