e6c4de63fed9c6603b91d07787bdc372d35aff1d
Krasimir Tsonev <krasimir@outset.ws>
2013-06-23 11:02:25 +0300

    Improving tree command

--- src/js/app-commands.js
+++ src/js/app-commands.js
@@ -1177,17 +1177,22 @@ Commands.register("shell", {
 var TreeCommandIsSoketAdded = false;
 Commands.register("tree", {
 	requiredArguments: 0,
-	format: '<pre>tree [regex]</pre>',
+	format: '<pre>tree [regex or deep]</pre>',
 	lookForQuotes: true,
 	concatArgs: true,
 	run: function(args, callback) {
 		var self = this;
+		var deep = -1;
 		var regex = args.length > 0 ? args.shift() : '';
 		regex = typeof regex != 'string' ? '' : regex;
+		if(!isNaN(regex)) {
+			deep = parseInt(regex);
+			regex = '';
+		}		
 		if(Shell.connected() && Shell.socket()) {
 			var onTreeDataReceived = function(res) {
 				Shell.socket().removeListener("tree", onTreeDataReceived);
-				if(res.result) self.formatResult(res.result, regex);
+				if(res.result) self.formatResult(res.result, regex, deep);
 				callback();
 			}
 			Shell.socket().on("tree", onTreeDataReceived);
@@ -1197,37 +1202,43 @@ Commands.register("tree", {
 			callback();
 		}
 	},
-	formatResult: function(dirs, regex) {
+	formatResult: function(dirs, regex, deep) {
 
 		var result = '',
 			self = this,
 			id = _.uniqueId("treeoutput");
 			r = regex != '' ? new RegExp(regex, 'gi') : false;
 
-		var formatItem = function(name, item, skipIndenting, path) {
-			if(r && !path.match(r) && path != './') return '';
-			var res = '';			
+		var formatItem = function(name, item, skipIndenting, path, currentDepth) {
+			if(deep > 0 && currentDepth == deep) {
+				return '';
+			}
+			var res = '',
+				visible = regex != '' ? (path + '/' + name).match(r) : true;
 			res += '<div class="' + (skipIndenting ? 'tree-wrapper' : 'tree-wrapper tree-wrapper-indent') + '">';
 			if(typeof item == "object") { // directory
 				var subfolders = !self.isEmpty(item);
 				res += '\
-					<div class="tree-item">\
+					<div class="tree-item tree-item-dir' + (visible ? '' : ' tree-item-hidden') + '">\
 						<a href="\
 							javascript:exec(\'shell cd ' + path + name + ' && tree\');\
 							document.getElementById(\'' + id + '\').style.display=\'none\';\
 						"><span>\
 						<i class="icon-folder' + (subfolders ? '-open' : '') + '"></i>\
-						' + name + '</span></a>\
+						' + name + '\
+						</span></a>\
 					</div>\
 				';
 				for(var i in item) {
-					res += formatItem(i, item[i], false, path + name + "/");
+					res += formatItem(i, item[i], false, path + name + "/", currentDepth+1);
 				}
 			} else {  // file
 				var filePath = (Context.get().replace(/\\/g, '/') + '/' + path + name).replace(/\.\//g, '');
 				res += '\
-					<div class="tree-item">\
-						<a href="javascript:exec(\'shell ' + filePath + '\');"><span><i class="icon-right-open"></i>' + name + '</span></a>\
+					<div class="tree-item tree-item-file' + (visible ? '' : ' tree-item-hidden') + '">\
+						<a href="javascript:exec(\'shell ' + filePath + '\');"><span><i class="icon-right-open"></i>\
+						' + name + '\
+						</span></a>\
 					</div>\
 				';
 			}
@@ -1236,7 +1247,7 @@ Commands.register("tree", {
 		}
 
 		for(var name in dirs) {
-			result += formatItem(name, dirs[name], true, './');
+			result += formatItem(name, dirs[name], true, './', 0);
 		}
 
 		App.setOutputPanelContent('<div class="tree" id="' + id + '">' + result + '</div>');
