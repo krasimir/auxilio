59b149052afdca0b271e4c8c717d87bd5881c595
Krasimir Tsonev <krasimir@outset.ws>
2013-06-27 23:28:16 +0300

    Improving aliases

--- src/js/commands/data/alias.js
+++ src/js/commands/data/alias.js
@@ -13,6 +13,10 @@ Commands.register("alias", {
 			this.storeAliases(callback)
 			return;
 		}
+		if(name === "exportallplease") {
+			this.exportAliases(callback)
+			return;
+		}
 		exec("storage get aliases", function(data) {
 			if(data.aliases && data.aliases != "") {
 				self.aliases = JSON.parse(data.aliases);
@@ -32,11 +36,33 @@ Commands.register("alias", {
 				}
 			} else {
 				var str = '';
+				var names = [];
 				for(var i in self.aliases) {
-					str += '<h2>' + i + '</h2><pre>' + self.aliases[i] + '</pre>';
+					names.push(i);
+				}
+				names.sort();
+				for(var i=0; a = names[i]; i++) {
+					str += '<a href="#" data="' + a + '" class="js-alias-list-link">' + a + '</a> ';
+					str += '<a href="#" data="' + a + '" class="js-alias-list-link-view"><i class="icon-eye"></i><br /></a>';
 				}
 				if(str != '') {
 					exec('echo ' + str);
+					setTimeout(function() {	
+						var links = document.querySelectorAll(".js-alias-list-link");
+						for(var i=0; link=links[i]; i++) {
+							link.addEventListener("click", function(e) {
+								exec(e.target.getAttribute("data"));
+							});
+						}
+						var linksView = document.querySelectorAll(".js-alias-list-link-view");
+						for(var i=0; linkView=linksView[i]; i++) {
+							(function(data, linkView) {
+								linkView.addEventListener("click", function(e) {
+									linkView.innerHTML = '<pre>' + self.aliases[data] + '</pre>';
+								});
+							})(linkView.getAttribute("data"), linkView);
+						}
+					}, 100);
 				} else {
 					exec('info No data.');
 				}
@@ -52,12 +78,35 @@ Commands.register("alias", {
 			});
 		});
 	},
+	exportAliases: function(callback) {
+		exec("storage get aliases", function(data) {
+			if(data.aliases && data.aliases != "") {
+				self.aliases = JSON.parse(data.aliases);
+				var names = [];
+				for(var i in self.aliases) {
+					names.push(i);
+				}
+				names.sort();
+				var result = '';
+				for(var i=0; name=names[i]; i++) {
+					result += 'alias ' + name + ' ' + self.aliases[name].toString().replace(/\n/g, "\\n") + '\n';
+				}
+				if(result == '') {
+					exec('info No data.');
+				} else {
+					exec('info <pre>' + result + '</pre>')
+				}
+			}
+			callback();
+		});
+	},
 	man: function() {
 		return 'Managing aliases.<br />\
 		alias - brings all the aliases<br />\
 		alias [name] - openes an editor for adding or editing. If you leave the textarea empty and click \'OK\', the alias will be deleted.<br />\
 		alias [name] [value] - directly pass the content of the alias<br />\
-		alias clearallplease - clears all the added aliases\
+		alias clearallplease - clears all the added aliases<br />\
+		alias exportallplease - exports all the aliases\
 		';
 	}	
 })
