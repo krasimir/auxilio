c9cd3d86b62f12ce3561c0934684c98543227030
Krasimir Tsonev <krasimir@outset.ws>
2013-07-07 22:35:46 +0300

    Organizing global functions

--- src/js/boot.js
+++ src/js/boot.js
@@ -1,4 +1,3 @@
-// Boot
 window.onload = function() {
 	Autocomplete.init();
 	Autocomplete.on("match", function(res) {
@@ -14,33 +13,3 @@ window.onload = function() {
 		}
 	});
 };
-
-// shortcuts
-var exec = function(commandStr, callback, arg) {
-	App.execute(commandStr, callback, arg);
-}
-// console.log in the background page
-var bglog = function(obj) {
-	if(chrome && chrome.runtime) {
-		chrome.runtime.sendMessage({type: "bglog", obj: obj});
-	}
-}
-// change the current directory
-var gotoClick = function(e) {
-	if(e.target.getAttribute("data-type") === "file") {
-		exec('shell ' + e.target.getAttribute("data-path"));
-	} else {
-		exec('shell cd ' + e.target.getAttribute("data-path"));
-	}
-}
-var attachGotoEvents = function() {
-	var gotolinks = document.querySelectorAll(".goto");
-	for(var i=0; link = gotolinks[i]; i++) {
-		(function(link){
-			link.removeEventListener("click", gotoClick)
-			link.addEventListener("click", gotoClick);
-		})(link);
-	};
-	setTimeout(attachGotoEvents, 500);
-}
-attachGotoEvents();
---
+++ src/js/global.js
@@ -0,0 +1,36 @@
+var exec = function(commandStr, callback, arg) {
+	App.execute(commandStr, callback, arg);
+}
+// console.log in the background page
+var bglog = function(obj) {
+	if(chrome && chrome.runtime) {
+		chrome.runtime.sendMessage({type: "bglog", obj: obj});
+	}
+}
+// change the current directory
+var gotoClick = function(e) {
+	if(e.target.getAttribute("data-type") === "file") {
+		exec('shell ' + e.target.getAttribute("data-path"));
+	} else {
+		exec('shell cd ' + e.target.getAttribute("data-path"));
+	}
+}
+var attachGotoEvents = function() {
+	var gotolinks = document.querySelectorAll(".goto");
+	for(var i=0; link = gotolinks[i]; i++) {
+		(function(link){
+			link.removeEventListener("click", gotoClick)
+			link.addEventListener("click", gotoClick);
+		})(link);
+	};
+	setTimeout(attachGotoEvents, 500);
+}
+var chain = function(funcs, callback) {
+	if(funcs.length === 0) {
+		callback();
+		return;
+	}
+	var f = funcs.shift();
+	f();
+}
+attachGotoEvents();
