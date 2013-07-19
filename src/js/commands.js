d37f3bd540f5a71c5e30b7c2ecc1fe3143b78929
Krasimir Tsonev <krasimir@outset.ws>
2013-07-19 11:21:04 +0300

    Fixing diff command

--- src/js/commands.js
+++ src/js/commands.js
@@ -150,45 +150,38 @@ Commands.register("diff", {
 	lookForQuotes: true,
 	concatArgs: true,
 	run: function(args, callback) {
-		if(args.length === 0) {
+		if(args.length >= 2) {
 			var self = this;
-			exec("readfiles raw", function(res) {
-				if(!res) {
-					callback();
-					return;
-				}
-				if(res.length === 2) {
-					var file1Ext = self.getExt(res[0].file.name);
-					var file2Ext = self.getExt(res[1].file.name);
+			var file1 = args[0];
+			var file2 = args[1];
+			exec("readfile " + file1, function(resFile1) {
+				exec("readfile " + file2, function(resFile2) {
+					var file1Ext = self.getExt(file1);
+					var file2Ext = self.getExt(file2);
 					if(file1Ext === file2Ext && file1Ext === "json") {
 						var file1JSON, file2JSON;
 						try {
-							file1JSON = JSON.parse(res[0].content);
+							file1JSON = JSON.parse(resFile1);
 						} catch(e) {
-							exec("error File " + res[0].file.name + " contains invalid json.");
+							exec("error File " + file1 + " contains invalid json.");
 							callback();
 							return;
 						}
 						try {
-							file2JSON = JSON.parse(res[1].content);
+							file2JSON = JSON.parse(resFile2);
 						} catch(e) {
-							exec("error File " + res[1].file.name + " contains invalid json.");
+							exec("error File " + file2 + " contains invalid json.");
 							callback();
 							return;
 						}
-						console.log(file1JSON, file2JSON);
 						self.compareJSON(file1JSON, file2JSON, callback);
 					} else {
-						self.compareText(res[0].content, res[1].content, callback);
+						self.compareText(resFile1, resFile2, callback);
 					}
-				} else {
-					exec("error <i>diff</i> works with two files.");
-				}
+				})
 			});
-		} else if(args.length === 2) {
-			this.compareText(args.shift(), args.shift(), callback);
-		} else if(args.length > 0) {
-			exec("error Sorry, but <i>diff</i> requires two or no arguments.")
+		} else {
+			exec("error Sorry, but <i>diff</i> requires two arguments.")
 		}
 		// this.compareText("Krasimir Tsonev is web developer", "Krasimir tyonev is developer", callback);
 		// this.compareJSON({a: 20}, {a: 20, b: { c: 20 }}, callback);
@@ -205,9 +198,10 @@ Commands.register("diff", {
 		// dmp.diff_cleanupSemantic(d);
 		// dmp.diff_cleanupEfficiency(d);
 		var result = '';
-		result += 'Difference:<pre>' + dmp.diff_prettyHtml(d) + '</pre>';
-		result += 'Text #1:<pre>' + t1 + '</pre>';
-		result += 'Text #2:<pre>' + t2 + '</pre>';
+		var diff = dmp.diff_prettyHtml(d);
+		result += 'Difference:<pre>' + diff + '</pre>';
+		// result += 'Text #1:<pre>' + t1 + '</pre>';
+		// result += 'Text #2:<pre>' + t2 + '</pre>';
 		exec("echo " + result);
 		callback(d);
 	},
@@ -216,13 +210,11 @@ Commands.register("diff", {
 		return parts[parts.length-1].toLowerCase();
 	},
 	man: {
-		desc: 'Comparison of files (text and json) or strings.',
-		format: 'diff<br />diff [string1] [string2]',
+		desc: 'Comparison of files (text and json).',
+		format: 'diff [file1] [file2]',
 		examples: [
-			{text: 'Opens a browse window for picking two files', code: 'diff'},
-			{text: 'Comparing two strings', code: 'diff "Hello world!" "Hello world, dude!"'},
-			{text: 'Command line (chaining)', code: 'date true && read monthName && diff "Current month is July"'},
-			{text: 'In script', code: 'diff(\'"Hello world!"\', \'"Hello world dude!"\', function(res) {\n\
+			{text: 'Compare two files', code: 'diff ./file1.txt ./file2.txt'},
+			{text: 'In script', code: 'diff(\'"file1.txt"\', \'"file2.txt"\', function(res) {\n\
 	console.log(res);\n\
 })'}
 		],
