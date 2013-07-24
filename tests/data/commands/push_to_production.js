64e05cf9b269bffd1dc0fe059f1d2908c79ad73a
Krasimir Tsonev <krasimir@outset.ws>
2013-07-24 19:31:36 +0300

    Test case

---
+++ tests/data/commands/push_to_production.js
@@ -0,0 +1,10 @@
+function nameOfCommand(args, callback) {
+	var commitMessage = args.length > 0 ? args.join(" ") : false;
+	if(commitMessage === false) {
+		error("Sorry, please type a commit message.");
+		callback();
+	} else {
+		echo("Pushing to production");
+		callback();
+	}    
+}
